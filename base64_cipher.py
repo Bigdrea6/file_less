0e281ecd2d0158273812f89b4f0eb8ba6e3084de
Bigdrea6 <hiro.mu.mu.166.k@gmail.com>
2022-03-13 15:02:26 +0900

    add base64 decoder

---
+++ base64_cipher.py
@@ -0,0 +1,14 @@
+import base64
+import re
+
+data = '[System.Convert]::FromBase64String("ZXhhbXBsZQ==")'
+
+def base64_dec(content_data):
+    if re.search(r".+base64.+", content_data, re.IGNORECASE):
+        enc_data = re.findall(r"\".+\"", content_data)
+        return base64.b64decode(enc_data[0][1:-1])
+
+
+dec_data = base64_dec(data)
+print(dec_data.decode())
+# example
