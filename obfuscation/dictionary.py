efcd3209c3daa4e661a87fc52a2c48079efedfc6
side_realms <87800512+doiuec@users.noreply.github.com>
2022-04-20 17:28:29 +0900

    Update dictionary.py

--- obfuscation/dictionary.py
+++ obfuscation/dictionary.py
@@ -1,17 +1,37 @@
 import re
+import ascii
+import Back_Ticks as back
+import base64_decode as base 
+import plus_division as plus
+import Reorder as reorder
+import replace
+import gzip_dec as gzip
 
-data = '[char]84+his ("{1}{0}" -f"s","i") an dxampld.replace("d","e") [System.Convert]::FromBase64String("ZXhhbXBsZQ==")'
+data = '[char]84+his ("{1}{0}" -f"s","i") an "dxampld".replace("d","e") [System.Convert]::FromBase64String("ZXhhbXBsZQ==")'
 
 def check(data):
-    if re.search(r".*base64.*", data, re.IGNORECASE):
-        print("base64")
-    if re.search(r".*gzip.*", data, re.IGNORECASE):
-        print("gzip")
+    if re.search(r".*'gzip'.*", data, re.IGNORECASE):
+        data = gzip.gzip_dec(data)
+    if re.search(r"\[System.Convert\].+base64.+\)", data, re.IGNORECASE):
+        # print("base")
+        data = base.base64_dec(data)
+        #print(data)
     if re.search(r".*\[char\].*", data, re.IGNORECASE):
-        print("ascii")
+        #print("char")
+        data = ascii.formatAscii(data)
+        #print(data)
     if re.search(r".*\.replace.*", data, re.IGNORECASE):
-        print("replace")
+        data = replace.formatReplace(data)
+        #print(data)
+    if re.search(r"\"(\{\d\})+\"", data, re.IGNORECASE):
+        #print("order")
+        data = reorder.formatReorder(data)
+        #print(data)
     if re.search(r"\'\+\'", data, re.IGNORECASE):
-        print("plus")
+        data = plus.formatPlus(data)
+        print(data)
+    if re.search(r"\'\+\'", data, re.IGNORECASE):
+        data = back.backticks(data)
+    print(data)
 
 check(data)
