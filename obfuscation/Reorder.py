94a919c7a1cf307943a62598322067c67ef708eb
side_realms <87800512+doiuec@users.noreply.github.com>
2022-04-20 17:29:45 +0900

    Update Reorder.py

--- obfuscation/Reorder.py
+++ obfuscation/Reorder.py
@@ -2,44 +2,36 @@
 
 import re
 
-data = 'This is an ("{1}{0}{2}" -F"AMP","EX", "LE")'
 
-def formatReplace(contentData):
+def formatReorder(contentData):
 
-    if(re.search(r"-f", contentData, re.IGNORECASE)):
+    if(re.search(r"-(f|F)", contentData)):
         exchange_index = re.findall(r"\"(\{.*\})\"", contentData)
         # print("hey")
-        print(exchange_index)
+        # print(exchange_index)
         exchange_index = (re.findall(r"\{\d\}", exchange_index[0]))
         brackets = "\{\}"
         for i in range(len(exchange_index)):
             for x in range(len(brackets)):
                 exchange_index[i] = exchange_index[i].replace(brackets[x],'')
-        print(exchange_index)
+        #print(exchange_index)
         # ['1', '0', '2']
 
-        exchange_sentence = re.findall(r"-f\".*\)", contentData, re.IGNORECASE)
+        exchange_sentence = re.findall(r"-f\".*\)", contentData)
         exchange_components = re.findall(r"\".*?\"", exchange_sentence[0])
-        print(exchange_components)
+        #print(exchange_components)
         # ['"AMP","EX", "LE"']
 
         exchanged = list()
 
         for entry in exchange_index:
             exchanged.append(exchange_components[int(entry)])
-        print(exchanged)
+        #print(exchanged)
         # ['"EX"', '"AMP"', '"LE"']
 
         exchanged = ''.join(exchanged).replace('\"','')
-        print(exchanged)
+        #print(exchanged)
         # EXAMPLE
 
-        raw_content = re.findall(r".*\(\"\{", data)
-        raw_content = raw_content[0].strip("\(\"\{")
-        print(raw_content)
-
-        
-
-
-
-formatReplace(data)
+        raw_content = re.sub(r"\(\"(\{\d\})+?\"\s-f\"\w\"(,\"\w\")*\)", exchanged, contentData)
+        return(raw_content)
