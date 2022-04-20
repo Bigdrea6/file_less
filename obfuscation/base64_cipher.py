e43cfed5afb0c148e70c3fe3c81417f75866c11f
side_realms <87800512+doiuec@users.noreply.github.com>
2022-04-20 17:29:26 +0900

    Update base64_cipher.py

--- obfuscation/base64_cipher.py
+++ obfuscation/base64_cipher.py
@@ -4,11 +4,13 @@ import re
 data = '[System.Convert]::FromBase64String("ZXhhbXBsZQ==")'
 
 def base64_dec(content_data):
-    if re.search(r"\[System.Convert\].+base64.+\)", content_data, re.IGNORECASE):
-        enc_data = re.findall(r"\".+\"", content_data)
-        return base64.b64decode(enc_data[0][1:-1])
-
-
-dec_data = base64_dec(data)
-print(dec_data.decode())
-# example
+    enc_data = re.findall(r"\[System.Convert\].+base64.+\)", content_data, re.IGNORECASE)
+    #print(enc_data[0])
+    enc_data = re.findall(r"\".+\"", enc_data[0])
+    #print(enc_data[0])
+    result =  base64.b64decode(enc_data[0][1:-1])
+    result = result.decode()
+    #print(type(result))
+    #print(result)
+    after_replace = re.sub(r"\[System.Convert\].+FromBase64String\(\".+\"\)", result, content_data)
+    return(after_replace)
