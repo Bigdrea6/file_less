00dea02095d0cf8432b108c6b7b453c915101c32
side_realms <87800512+doiuec@users.noreply.github.com>
2022-05-24 20:02:12 +0900

    Update backTicks.py

--- obfuscation/backTicks.py
+++ obfuscation/backTicks.py
@@ -1,11 +1,12 @@
 import re
 
-
 simbol = ["$", "&", "'", '"', "'" ]
 
+result = []
+
 def backticks(content_data):
-    result = []
     content_data = list(content_data)
+    result = []
 
     for i in range(len(content_data)):
         if content_data[i] == "`":
@@ -13,14 +14,16 @@ def backticks(content_data):
                 del content_data[i+1]
                 content_data.append(" ")
                 i += 1
-
             elif content_data[i+1] == "\\" and content_data[i+2] == "n":
                 result.append(content_data[i])
-                
+            elif i+1 == len(content_data):
+                result.append("\n")
+
             else:
                 pass
+                
         else:
             result.append(content_data[i])
 
     after_replace = str(result)
-    return (after_replace)
+    return(after_replace)
